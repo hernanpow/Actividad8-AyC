982fb62c1b7bb2e91a584bfb16a3e4889be118f7
hernanpow <perezhernan59@gmail.com>
2021-05-31 00:45:55 -0300

    First PUSH

---
+++ src/Algoritmos/Ysinordenar.java
@@ -0,0 +1,18 @@
+package Algoritmos;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Ysinordenar {
+
+	public static Par 
+	
+	
+	
+	
+	
+	public static List<Punto> Xordenado(List<Punto> l){
+		Comparator<Punto> comparador = new Comparator<Punto>();
+		
+	}
+}
